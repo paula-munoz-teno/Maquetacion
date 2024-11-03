0fdec416457baf21696bdddddcb04036ebefcc11
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-11-03 15:52:22 +0100

    subida actualización r2dia7

--- Maquetación_basica/reto72.js
+++ Maquetación_basica/reto72.js
@@ -1,16 +1,157 @@
+    
+    //Cuando de a carrito me muestre la lista de la compra y el precio final 
+
+//A. Cuando le de a carrito me muestre el contenedor
+
+    
+//     function mostrar(){
+//         document.getElementById("carrito").style.display="block";
+//     }
+
+// //B. Cuando le de a la X, desaparece el contenedor 
+
+//     function ocultar(){
+//         document.getElementById("carrito").style.display="none";
+//     }
+    
+
+//C. Total de compras mostrará la suma de las prendas que se hayan añadido
+
+
+
+
+    //Cuando pinche en el botón, se añade a ítem del carrito esta información
+    //Para cada uno de los botones 
+
+
+//Código general (precio y prenda) NO ES MOSTRAR ES AÑADIR 
+
+// function mostrarp1(){
+//     document.getElementById("pp1").style.display="block";
+// }
+
+
+function add1(){
+
+    let elemento = document.getElementById("pp1");
+    elemento.innerHTML += "<p>Pantalón suelto gris</p>";
+    elemento.innerHTML += <p>20</p>;
+}
+
+   
+
+
+     
+// let solicitud = arrayfiltrado[i];
+// let elemento = document.getElementById("op");
+// elemento.innerHTML += "<p>Nombre:</p>";
+// elemento.innerHTML += solicitud.nombre;
+// elemento.innerHTML += "<p>   </p>";
+
+
+
+//BOTÓN I 
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // Escribir una función en JavaScript que recoja la información de los campos de texto y cree un
     // objeto literal que se guarde en un array de objetos literales cada vez que se pulse el botón
     // "solicitar información".
+
+
+    //Hacer esto por cada prenda: poner un identificador id y crear un objeto literal para cada prenda luego  
+//No porque no estás en un formulario. Trabajar con los valores. Si pincho este botón, añado: Prenda x 12 
+//al contendor 
+//Si pulso el carrito se abre el contenedor y sumo la cantidad 
+
+
     
-    let array =[];
 
-    function recojotInfo() {
-    const nombreprenda = document.getElementById('nombreprenda').value;
-    const precio = document.getElementById('precio').value;
 
-    let objetoliteral ={nombreprenda:nombreprenda, precio:precio};
 
-    array.push(objetoliteral);
+//     let array =[];
+
+//     function requestInfo() {
+
+       
+//     let prenda = document.getElementsByClassName("prenda").value;
+//     // console.log(nombreprenda)
+//     // console.log(prenda)
+
+//     console.log(prenda)
+   
+//     let precio = document.getElementsByClassName("precio").value;
+//     console.log(precio)
+
+
+//     let objetoliteral ={nombreprenda:prenda, precio:precio};
+
+//     array.push(objetoliteral);
     
-    console.log(array);
-}
+//     console.log(array);
+// }
+
+
+
+
+
+
+
+
+    // let prenda = document.getElementsByClassName("prenda");
+    // console.log(prenda.value)
+    
+    
+//     let array =[];
+
+//     function requestInfo() {
+
+       
+//     let prenda = document.getElementsByClassName("prenda").value;
+//     // console.log(nombreprenda)
+//     // console.log(prenda)
+
+//     console.log(prenda)
+   
+//     let precio = document.getElementsByClassName("precio").value;
+//     console.log(precio)
+
+
+//     let objetoliteral ={nombreprenda:prenda, precio:precio};
+
+//     array.push(objetoliteral);
+    
+//     console.log(array);
+// }
