a573731a68ca12f2632027474ab0a2bec178a8ba
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-12-11 15:45:18 +0100

    subir borrador dia5M2

---
+++ teoria.js
@@ -0,0 +1,25 @@
+//REPONSIVE DESIGN 
+
+//UX: diseño visual (user experience)
+//UI: diseño (interfaz del usuario)
+
+//tamaños de pantallas
+
+//MÓVILES
+//ancho: 280px-475px
+
+//TABLETS
+//ancho: 476px-768px
+
+//ORDENADORES
+// >769px
+
+
+//A NIVEL PRÁCTICO: 
+//1ºse decide qué es lo más probable de uso
+//se hace la maquetación para ese uso
+//se adapta a otros tamaños de pantalla 
+
+
+//RETOS
+//Usar bootsrap
