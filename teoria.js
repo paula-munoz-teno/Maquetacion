700d75422e01295c6c98a61f20c4d5d29d9c8200
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-12-12 11:53:36 +0100

    subir retos dia5

--- teoria.js
+++ teoria.js
@@ -20,6 +20,32 @@
 //se hace la maquetación para ese uso
 //se adapta a otros tamaños de pantalla 
 
+//SINTAXIS
+
+//@media: empezar la media query
+//-media types: all, print, screen (es para modo de impresión, para webs screen)
+
+
+//op lógicos: and y not (cuando la pantalla sea así X)
+//EJ: max-width and max-height
+//Cuando el dispositivo tenga como máximo x ancho y x alto
+
+//Encapsulado: lo que va a ocurrir si se cumplen las condiciones 
+
+//VER TAMAÑOS DE RESPONSIVE
+//botón derecho
+//Inspeccionar
+//Icono de ordenador grande y móvil
+//dimensiones reactivo (para adaptarse)
+
+
+//CONSIDERACIONES
+//Lo que no se modifique se hereda
+
+
+
 
 //RETOS
 //Usar bootsrap
+
+
